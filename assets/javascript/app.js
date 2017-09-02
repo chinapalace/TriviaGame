bb3e13d0bf5a7d0bdd0ba42e80fc78f0700d5568
Clay Fudurich <clayfudurich@Justins-Macbook.local>
2017-09-01 18:39:20 -0700

    added responses for correct and incorrect answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,59 +1,120 @@
-// array of question objects 
-var questions = [sleep, fins, largestMammal, blowhole];
+// click events 
+// window.onload = function() {
+
+  $(".answer1").on("click", correctChoice);
+  $(".answer2").on("click", wrongChoice);
+  $(".answer3").on("click", wrongChoice);
+  $(".answer4").on("click", wrongChoice);
+
+// };
+// var to keep track of correct answer
+var score = 0;
+// var to keep track of questions
+var questionCount = 0;
 
 // question objects
-var sleep = {
+ var sleep = {
 
-	ask: "How many hours a day are whales thought to sleep?"
-	c: "8 hours"
-	a: "6 hours"
-	a: "4 hours"
-	a: "10 hours"
+	ask: "How many hours a day are whales thought to sleep?",
+	correctAnswer: "8 hours",
+	a1: "6 hours",
+	a2: "4 hours",
+	a3: "10 hours",
 
-	image: '../images/animatedWhales.mp4'
+	image: 'assets/images/animatedWhales.gif'
 
 };
 
 var fins = {
 
-	ask: "What is the name of a whale's forelims?"
-	c: "pectoral fins"
-	a: "dorsal fins"
-	a: "paddle fins"
-	a: "flukes"
+	ask: "What is the name of a whale's forelims?",
+	c: "pectoral fins",
+	a1: "dorsal fins",
+	a2: "paddle fins",
+	a3: "flukes",
 
-	image: "../images/nemoWhales.mp4"
+	image: "assets/images/nemoWhales.gif"
 };
 
 var largestMammal = {
 
-	ask: "Which whale is the largest know mammal?"
-	c: "Blue Whale"
-	a: "Grey Whale"
-	a: "Orca"
-	a: "Sperm Whale"
+	ask: "Which whale is the largest known mammal?",
+	c: "Blue Whale",
+	a1: "Grey Whale",
+	a2: "Orca",
+	a3: "Sperm Whale",
 
-	image: "../images/whales.mp4"
+	image: "assets/images/whales.gif"
 };
 
 var blowhole = {
 
-	ask: "Which whale has two blowholes?"
-	c: "Baleen Whale"
-	a: "Pilot Whale"
-	a: "Toothed Whale"
-	a: "Orcas"
+	ask: "Which whale has two blowholes?",
+	c: "Baleen Whale",
+	a1: "Pilot Whale",
+	a2: "Toothed Whale",
+	a3: "Orcas",
 
-	image "../images/whalesAbove.mp4"
+	image: "assets/images/whalesAbove.gif"
 };
+
+// array of question objects
+var questions = [sleep, fins, largestMammal, blowhole]
 
 
 // question randomizer
 var question = questions[Math.floor(Math.random()*questions.length)];
-// show new question
-function displayQuestion() = {
+console.log(questions);
+console.log(Math.floor(Math.random()*questions.length))
+console.log(question)
+console.log(fins.ask)
+console.log(question['ask'])
+console.log(question['a'])
 
-  $(".question").text(question.ask);
-}
+
+displayQuestion()
+displayAnswers()
+
+// show new question
+function displayQuestion()	{
+
+  $(".question").html(question.ask);
+};
+
+// show answer choices 
+function displayAnswers()	{
+	$(".answer1").html(question.c);
+	$(".answer2").html(question.a1);
+	$(".answer3").html(question.a2);
+	$(".answer4").html(question.a3);
+};
+
+// correct answer selected
+function displayCorrect()	{
+	$('.questions').hide();
+	$('#image-holder').append('<p>Correct!</p>')
+	.append("<img src=" + question['image'] + " width='480' height='360' frameBorder='0'>");
+};
+
+function correctChoice()	{
+	score++;
+	console.log(score)
+	questionCount++;
+	displayCorrect();
+};
+
+
+// wrong answer selected
+function displayIncorrect()	{
+	$('.questions').hide()
+	$('#image-holder').append('<p>Wrong!</p>')
+	.append("<img src='assets/images/wrongAnswer.gif'>");
+	
+};
+
+function wrongChoice()	{
+	questionCount++;
+	displayIncorrect();
+};
 
 
